540ba4822c4415120173628b519202e657f0b60a
GNUMORPH <mohsenfarrahiii@gmail.com>
2025-10-22 22:20:33 +0330

    Add files via upload

---
+++ مجزا.py
@@ -0,0 +1,15 @@
+#https://quera.org/problemset/129726
+
+def separator(ls):
+    pass
+    
+    zoj=[]
+    fard=[]
+    for i in ls:
+        if i%2==0:
+            zoj.append(i)
+        else:
+            fard.append(i)
+
+    return (zoj,fard)
+
