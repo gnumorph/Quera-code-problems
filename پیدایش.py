2b5ed33b3cdfe83d6381c759dc8f764b25656b71
GNUMORPH <mohsenfarrahiii@gmail.com>
2025-10-23 07:20:18 +0330

    Add files via upload

---
+++ پیدایش.py
@@ -0,0 +1,9 @@
+#https://quera.org/problemset/102250
+
+def find(num1,num2,num3):
+    pass
+    digits=[num1,num2,num3]
+    for i in range(1,5):
+        if i not in digits:
+            return i
+
